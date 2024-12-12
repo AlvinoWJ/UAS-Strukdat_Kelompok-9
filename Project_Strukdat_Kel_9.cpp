3ec91f4ec35a39fcbe41be93968c945dd1c4ce98
AlvinoWJ <Alvinowj@gmail.com>
2024-12-12 13:53:34 +0700

    coba2

--- Project_Strukdat_Kel_9.cpp
+++ Project_Strukdat_Kel_9.cpp
@@ -1,4 +1,11 @@
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
+#include <string.h>
 
+// Struktur untuk Node Double Linked List (Pemilihan Kelas)
+typedef struct Kelas
+{
+    char nama[50];
+    struct Kelas *prev;
+    struct Kelas *next;
+} Kelas;
