b8eb8ebdf286c5420700f418d03d8b1a6684ee12
DPutra17 <effandyputra45@gmail.com>
2024-12-13 21:34:48 +0700

    queue_1

--- Project_Strukdat_Kel_9.cpp
+++ Project_Strukdat_Kel_9.cpp
@@ -3,13 +3,6 @@
 #include <string.h>
 
 
-
-int main(){
-
-    return 0;
-}
-
-
 // Struktur untuk Node Double Linked List (Pemilihan Kelas)
 typedef struct Kelas
 {
@@ -20,3 +13,60 @@ typedef struct Kelas
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+typedef struct node{
+    char materi[50];
+    struct node *next;
+} node;
+
+typedef struct queue {
+    node *front;
+    node *rear;
+} queue;
+
+void define(queue *q){
+    q->front = NULL;
+    q->rear = NULL;
+}
+
+void enqueue(const char *materi, queue *q){
+    node *pNew = (node *)malloc(sizeof(node));
+
+    strcpy(pNew->materi, materi);
+    pNew->next = NULL;
+
+    if (q->rear != NULL)
+        q->rear->next = pNew;
+    else 
+        q->front = pNew;
+
+    q->rear = pNew;
+}
+
+void dequeue(queue *q){
+    if (q->front == NULL){
+        printf("tidak ada materi lagi.\n");
+        return;
+    }
+
+    node *temp = q->front;
+    q->front = q->front->next;
+    if (q->front == NULL)
+        q->rear = NULL;
+
+    printf("materi telah selesai.\n", temp->materi);
+    free(temp);
+}
